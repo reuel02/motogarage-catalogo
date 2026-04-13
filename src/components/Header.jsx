2c6507d97ad11c0ce21af1c743c79d3512b3ac73
Reuel Ferreira <ferreirareuel3@gmail.com>
2026-04-13 17:26:28 -0300

    Ajustando responsividade

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,34 @@
 import { FaInstagram, FaShoppingCart } from "react-icons/fa";
 
-
 export default function Header() {
     return (
-        <div className="flex flex-row justify-between items-center h-38 bg-zinc-900 text-zinc-200 antialiased border-b border-zinc-800">
-            <h1 className="ml-20 text-4xl text-zinc-200 font-light">Moto<span className="text-amber-500 font-bold">Garage</span></h1>
+        <header className="flex flex-col md:flex-row justify-between items-center bg-zinc-900 text-zinc-200 antialiased border-b border-zinc-800 px-6 py-6 lg:px-20 gap-6 md:gap-0">
+            
+            {/* Logo */}
+            <h1 className="text-4xl text-zinc-200 font-light text-center md:text-left">
+                Moto<span className="text-amber-500 font-bold">Garage</span>
+            </h1>
 
-            <div className="flex flex-row gap-5 items-center mr-10">
-                <div className="flex flex-row items-center gap-5 text-zinc-200 text-1xl mr-10">
-                    <a href="" className="hover:text-amber-500 transition-colors">Catálogo</a>
-                    <a href="" className="hover:text-amber-500 transition-colors">Sobre</a>
-                    <a href="" className="hover:text-amber-500 transition-colors">Contato</a>
+            {/* Box Direita (Navegação + Ícones) */}
+            <div className="flex flex-col sm:flex-row gap-6 sm:gap-10 items-center">
+                
+                {/* Links */}
+                <nav className="flex flex-row gap-6 text-base font-medium">
+                    <a href="#" className="hover:text-amber-500 transition-colors">Catálogo</a>
+                    <a href="#" className="hover:text-amber-500 transition-colors">Sobre</a>
+                    <a href="#" className="hover:text-amber-500 transition-colors">Contato</a>
+                </nav>
+
+                {/* Ícones */}
+                <div className="flex flex-row gap-5">
+                    <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+                        <FaInstagram className="size-6 lg:size-7 hover:text-amber-500 transition-colors" />
+                    </a>
+                    <a href="#" target="_blank" rel="noreferrer">
+                        <FaShoppingCart className="size-6 lg:size-7 hover:text-amber-500 transition-colors" />
+                    </a>
                 </div>
-
-                <a href="https://www.instagram.com/" target="_blank"><FaInstagram className="size-8 hover:text-amber-500" /></a>
-                <a href="#" target="_blank"><FaShoppingCart className="size-8 hover:text-amber-500" /></a>
             </div>
-        </div>
-
+        </header>
     )
 }
